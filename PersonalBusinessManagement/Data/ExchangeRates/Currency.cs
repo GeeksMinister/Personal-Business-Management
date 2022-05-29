e778e4490eeb62280801d334371bf1c2cbb16051
GeeksMinister <nael_sps@hotmail.com>
2022-05-29 17:53:34 +0200

    Add project files.

---
+++ PersonalBusinessManagement/Data/ExchangeRates/Currency.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.Json.Serialization;
+
+namespace PersonalBusinessManagement.Data.ExchangeRates;
+
+public sealed class Currency
+{
+#pragma warning disable CS8618
+    [JsonPropertyName("updated")]
+    public string UpdateDate { get; set; }
+    [JsonPropertyName("USD_SEK")]
+    public double USD_SEK { get; set; }
+    [JsonPropertyName("EUR_SEK")]
+    public double EUR_SEK { get; set; }
+    [JsonPropertyName("GBP_SEK")]
+    public double GBP_SEK { get; set; }
+    [JsonPropertyName("CAD_SEK")]
+    public double CAD_SEK { get; set; }
+    [JsonPropertyName("CHF_SEK")]
+    public double CHF_SEK { get; set; }
+    [JsonPropertyName("JPY_SEK")]
+    public double JPY_SEK { get; set; }
+
+    public override string ToString()
+    {
+        return $"Updated:  {UpdateDate}\n" +
+            $"USD: {USD_SEK}\n" +
+            $"EUR: {EUR_SEK}\n" +
+            $"GBP: {GBP_SEK}\n" +
+            $"CAD: {CAD_SEK}\n" +
+            $"CHF: {CHF_SEK}\n" +
+            $"JPY: {JPY_SEK}\n";
+    }
+}
+
+
---
+++ PersonalBusinessManagement/Data/ExchangeRates/CurrencyFileHandler.cs
@@ -0,0 +1,51 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Text.Json;
+
+namespace PersonalBusinessManagement.Data.ExchangeRates;
+
+public static class CurrencyFileHandler
+{
+    private const string currencyFile = "Data\\ExchangeRates\\ExchangeRates.json";
+    public static Currency? GetExchangeRates()
+    {
+        var fileContent = File.ReadAllText(currencyFile);
+        var jsonObj = JsonSerializer.Deserialize<IEnumerable<Currency>>(fileContent);
+        return jsonObj?.FirstOrDefault();
+    }
+
+    public static async Task<string[]> GenerateLinks()
+    {
+        var apiKey = await File.ReadAllTextAsync("free - Copy.currconv.txt");
+        return new[]
+        {
+            "https://free.currconv.com/api/v7/convert?q=USD_SEK,EUR_SEK&compact=ultra&apiKey=" + apiKey,
+            "https://free.currconv.com/api/v7/convert?q=GBP_SEK,CAD_SEK&compact=ultra&apiKey=" + apiKey,
+            "https://free.currconv.com/api/v7/convert?q=CHF_SEK,JPY_SEK&compact=ultra&apiKey=" + apiKey,
+        };
+    }
+
+    public static async Task UpdateExchangeRates()
+    {
+        try
+        {
+            var client = new HttpClient();
+            var getRequests = await GenerateLinks();
+            JObject json = JObject.Parse($"{{ \"updated\": \"{DateTime.Now}\"}}");
+            for (int i = 0; i < getRequests.Length; i++)
+            {
+                var response = JObject.Parse(await client.GetStringAsync(getRequests[i]));
+                json.Merge(response);
+            }
+            await File.WriteAllTextAsync("ExchangeRates.json", $"[\n{json}\n]");
+        }
+        catch (Exception e)
+        {
+            File.WriteAllText($"ErrorReport.txt", "Error!  Couldn't update or write" +
+                 $" successfully {DateTime.Now}\n{e.Message}\n\n");
+        }
+    }
+
+
+
+}
+
