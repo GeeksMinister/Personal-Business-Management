650f45796102161682136be3e730a1320564e09c
GeeksMinister <nael_sps@hotmail.com>
2022-06-06 14:15:24 +0200

    Generated the 'Projects' Controller + pages. Based on the model 'Project' generated DB & table by EF Working on the Search feature.

---
+++ PersonalBusinessManagement/Data/PersonalBusinessManagementContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PersonalBusinessManagement.Models;
+
+namespace PersonalBusinessManagement.Data;
+
+#pragma warning disable CS8618
+public class PersonalBusinessManagementContext : DbContext
+{
+    public PersonalBusinessManagementContext(
+        DbContextOptions<PersonalBusinessManagementContext> options) :base(options) { }
+
+    public DbSet<Project> Project { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Ignore<Currency>();
+        modelBuilder.Ignore<ErrorViewModel>();
+        modelBuilder.Ignore<GithubRepo>();
+    }
+
+
+}
---
+++ PersonalBusinessManagement/Models/Project.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PersonalBusinessManagement.Models;
+
+public class Project
+{
+    [Key]
+    public int Id { get; set; }
+    [StringLength(100, ErrorMessage = "Name is too big. 100 charactera max!")]
+    public string Name { get; set; } = "Project Name";
+    [StringLength(1250, ErrorMessage = "1250 charactera max!")]
+    public string? Description { get; set; }
+    public bool Delivered { get; set; }
+    public Project()
+    {
+
+    }
+
+}
